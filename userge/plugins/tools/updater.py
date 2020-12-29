38ebd572673ae289de47e8b7e22c64ca07b830aa
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-12-29 15:19:13 +0000

    black autoformat

--- userge/plugins/tools/updater.py
+++ userge/plugins/tools/updater.py
@@ -153,4 +153,6 @@ def _heroku_helper(sent: Message, repo: Repo, branch: str) -> None:
             userge.loop.create_task(sent.try_to_edit(f"{cur_msg}\n\n{prog}"))
 
     cur_msg = sent.text.html
-    repo.remote("heroku").push(refspec=f"{branch}:master", progress=progress, force=True)
+    repo.remote("heroku").push(
+        refspec=f"{branch}:master", progress=progress, force=True
+    )
