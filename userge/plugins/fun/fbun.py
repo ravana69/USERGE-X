86606072d4b38b06772289645d7e9ebcd3bbac10
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-03-05 05:59:06 +0000

    black autoformat

--- userge/plugins/fun/fbun.py
+++ userge/plugins/fun/fbun.py
@@ -1,6 +1,8 @@
-import asyncio
 import random
+
 from userge import Message, userge
+
+
 @userge.on_cmd(
     "fbun",
     about={
@@ -9,4 +11,6 @@ from userge import Message, userge
     },
 )
 async def detail_hi(message: Message):
-  await message.edit(f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has been fbunned from: \n**1. Direct Message** : **Yes**\n**2. Public Group :** {random.choice(range(0,99))} Groups\n**3. Public Channel** : {random.choice(range(0,99))} Channels\n**4. Private Group :** {random.choice(range(0,99))} Groups\n**5. Private Channel :** {random.choice(range(0,99))} Channels")
+    await message.edit(
+        f"{(await userge.get_users(message.reply_to_message.from_user.id)).first_name} has been fbunned from: \n**1. Direct Message** : **Yes**\n**2. Public Group :** {random.choice(range(0,99))} Groups\n**3. Public Channel** : {random.choice(range(0,99))} Channels\n**4. Private Group :** {random.choice(range(0,99))} Groups\n**5. Private Channel :** {random.choice(range(0,99))} Channels"
+    )
